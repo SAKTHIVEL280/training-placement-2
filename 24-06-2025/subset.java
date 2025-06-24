28911e9803d6698a9bd4ff53208d96e0a74e4053
SAKTHIVEL_E <148361514+SAKTHIVEL280@users.noreply.github.com>
2025-06-24 20:14:42 +0530

    Create subset.java

---
+++ 24-06-2025/subset.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isSubset(int a[], int b[]) {
+        // Your code here
+        Arrays.sort(a);
+        Arrays.sort(b);
+        int count = 0;
+        int i = 0, j =0;
+        for(;i<a.length && j < b.length;i++){
+            if(a[i] == b[j]){
+                count++;
+                j++;
+            }
+        }
+        return count==b.length;
+    }
+}
