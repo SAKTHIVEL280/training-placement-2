b1de39884b82615b007cabed98ae428c65a2b1dc
SAKTHIVEL_E <148361514+SAKTHIVEL280@users.noreply.github.com>
2025-06-24 20:10:29 +0530

    Create sum of n.java

---
+++ 24-06-2025/sum of n.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static int findSum(int n) {
+        // code here
+        int r=0;
+        for(int i = 1;i<=n;i++)
+        {
+            r=r+i;
+        }
+        return r;
+    }
+}
